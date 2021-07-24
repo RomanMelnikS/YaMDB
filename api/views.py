11681e6fdfa1d95e4d2a2a9d9d14665b39007ef8
RomanMelnikS <melnikovichh@yandex.ru>
2021-07-24 11:13:20 +0500

    add yamdb_workflow test

--- api/views.py
+++ api/views.py
@@ -105,6 +105,7 @@ def send_confirmation_code(request):
             recipient_list=[email, ]
         )
         return Response('Confirmation code отправлен на ваш Email.')
+    return Response('Что-то пошло не так.')
 
 
 @api_view(['POST'])
