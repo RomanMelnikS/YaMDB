7e6bc2d9cdd539986336e37adb485e00ecb1813f
RomanMelnikS <melnikovichh@yandex.ru>
2021-07-25 14:13:51 +0500

    finaly

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -3,11 +3,13 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY', default='hgushdbgvjhhsdbvhshgyusdgyu536t3tr3wy237657ewtgfyuh')
+# SECRET_KEY = os.environ.get('SECRET_KEY', default='hgushdbgvjhhsdbvhshgyusdgyu536t3tr3wy237657ewtgfyuh')
+
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('IP'), os.environ.get('DOMIAN_NAME'), 'localhost', 'web',]
+ALLOWED_HOSTS = [os.environ.get('IP'), os.environ.get('DOMIAN_NAME'), 'localhost', 'web']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
